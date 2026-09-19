69b5eb516646a482b1fa6350fdadebeb80314561
mariam <yamm47968@Gmail.com>
2026-09-19 14:34:09 +0200

    solved exercise 1 of day 3

---
+++ day3/ex1.js
@@ -0,0 +1,8 @@
+// Greeting Function: Write a standard function greet(name) that takes a string and logs "Hello, [name]!
+function salution() {
+   return (" hello "); 
+}
+const p =require("prompt-sync")()
+let nom = p(" enter votre nom ")
+
+console.log(salution()+nom)
