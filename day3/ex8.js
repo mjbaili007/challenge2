9761cb2efe6c86ee67f7d3e847f6c9a680ac2a1e
mariam <yamm47968@Gmail.com>
2026-09-19 16:26:47 +0200

    add

--- day3/ex8.js
+++ day3/ex8.js
@@ -1,9 +1,11 @@
-/*Closure Intro: Write a function createMultiplier(multiplier) that returns a new arrow function.
- The returned function should take a number and multiply it by the original multiplie */
 
 const r = require ("prompt-sync")()
 let x  = r("donner moi une nbr");
 let y = r("donner moi  la 2 nbr");
 function calculate(a, b, operation) {
-    return operation
+    return operation(a,b) ;
 }
+let add = (x,y) => x + y ;
+let sou = (x,y) => x - y ;
+console.log("l'addition",calculate(x,y , add));
+console.log("la soustraction ",calculate(x,y , sou));
